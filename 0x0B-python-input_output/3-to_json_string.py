42be1ecc5e062b73e295aaac9222aa057057536f
Cecilia Mutero <cecilmutero66@gmail.com>
2024-02-27 01:37:14 -0800

    3. To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+returns the JSON representation of an object (string)
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Return the JSON representation of an object (string).
+
+    Parameters:
+    my_obj: The object to be converted to JSON.
+
+    Returns:
+    str: The JSON representation of the object.
+    """
+    return json.dumps(my_obj)
