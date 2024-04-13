ed04229e94c1ba15a1b6df6c4881d5fdf6e9aeef
Cecilia <cecilmutero66@gmail.com>
2024-04-12 21:08:07 -0700

    0. My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
 -- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
-USE mysql;
-SELECT * FROM user  WHERE user  = 'user_0d_1';
-SELECT *FROM user WHERE user = 'user_0d_2';
+SELECT * FROM user  WHERE user  = 'user_0d_1' AND host = 'localhost';
+SELECT *FROM user WHERE user = 'user_0d_2' AND host = 'localhost';
