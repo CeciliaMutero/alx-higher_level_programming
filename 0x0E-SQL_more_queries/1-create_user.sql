7756b4751b7763fb97dbb9910e1bcd1012bdccab
Cecilia <cecilmutero66@gmail.com>
2024-04-12 22:13:38 -0700

    1. Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,3 @@
 -- script that creates the MySQL server user user_0d_1.
--- Check if user_0d_1 already exists, if not, create the user
 CREATE user IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
--- Grant all privileges to user_0d_
-GRANT ALL PRIVELEDGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVELEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
