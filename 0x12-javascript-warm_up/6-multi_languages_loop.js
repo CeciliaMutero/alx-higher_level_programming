db3e35859689d2ca237d34048b6680f4bbf633c0
Cecilia <cecilmutero66@gmail.com>
2024-04-25 03:47:54 -0700

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let i = 0;
+const arrayofstrings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+while (i < arrayofstrings.length) {
+  console.log(arrayofstrings[i]);
+  i++;
+}
