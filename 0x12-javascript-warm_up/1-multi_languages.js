9aa3220d57c7bb3d8f8dabbdc965204b1c7df5db
Cecilia <cecilmutero66@gmail.com>
2024-04-24 19:09:47 -0700

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* print string */
+const myar = 'C is fun';
+const pyth = 'Python is cool';
+const jav = 'JavaScript is amazing';
+console.log(myar);
+console.log(pyth);
+console.log(jav);
