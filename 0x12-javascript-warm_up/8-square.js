6205bedd997a20d1b31629d6d76d29ef5e0c2f35
Cecilia <cecilmutero66@gmail.com>
2024-04-25 17:13:40 -0700

    8. Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,9 @@ const i = parseInt(process.argv[2]);
 if (isNaN(i)) {
   console.log('Missing size');
 } else {
-  for (let y = 0; y < i; y++) {
-    console.log('x'.repeat(i));
+  if (i > 0) {
+    for (let y = 0; y < i; y++) {
+      console.log('x'.repeat(i));
+    }
   }
 }
